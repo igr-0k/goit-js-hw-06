09066799ab9883e272313fefddff190a4f22064e
igr-0k <igor.01101110@gmail.com>
2022-09-17 22:31:49 +0300

    Task#2 - done!

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsList = document.querySelector('ul#ingredients');
+
+const ingredientItems = ingredients.map(ingredient => {
+  const ingredientEl = document.createElement('li');
+  ingredientEl.textContent = ingredient;
+  ingredientEl.classList.add('item');
+
+  return ingredientEl;
+});
+
+ingredientsList.append(...ingredientItems);
