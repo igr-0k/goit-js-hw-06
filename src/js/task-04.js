f0ed5edc957b4c3a3b2a38179b26067ceb33aea0
igr-0k <igor.01101110@gmail.com>
2022-09-18 03:31:34 +0300

    Task#4 - done!

---
+++ src/js/task-04.js
@@ -0,0 +1,15 @@
+const valueDisp = document.querySelector('#value');
+
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+
+let counterValue = 0;
+
+decrementBtn.addEventListener('click', () => {
+  valueDisp.textContent = `${(counterValue -= 1)}`;
+});
+
+incrementBtn.addEventListener('click', () => {
+  valueDisp.textContent = `${(counterValue += 1)}`;
+});
