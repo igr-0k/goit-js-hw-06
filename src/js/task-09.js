7a092b380104dff78a995eb9fd9ff168ef83c674
igr-0k <igor.01101110@gmail.com>
2022-09-19 22:53:19 +0300

    Task#9 - done!

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+
+const changeColorButton = document.querySelector('button.change-color');
+
+const spanEl = document.querySelector('span.color');
+
+changeColorButton.addEventListener('click', onClick);
+function onClick() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+}
