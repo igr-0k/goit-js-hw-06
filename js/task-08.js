73ab81b2de60313e27ba42765150d4a7d69a86fb
igr-0k <igor.01101110@gmail.com>
2022-09-20 07:50:42 +0300

    [edited]

--- js/task-08.js
+++ js/task-08.js
@@ -5,8 +5,8 @@ function onSubmit(event) {
   event.preventDefault();
   const { email, password } = event.currentTarget.elements;
   const data = {
-    email: email.value,
-    password: password.value,
+    email: email.value.trim(),
+    password: password.value.trim(),
   };
   if (email.value === '' || password.value === '') {
     alert('All fields must be completed!');
