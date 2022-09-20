3a7b4b1aa548108189e769371fcd3a0d61079232
igr-0k <igor.01101110@gmail.com>
2022-09-20 07:31:07 +0300

    Task#10 - done!

--- js/task-10.js
+++ js/task-10.js
@@ -2,25 +2,35 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-// Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит количество элементов в input и нажимает кнопку Создать, после чего рендерится коллекция. При нажатии на кнопку Очистить, коллекция элементов очищается.
+const divEl = document.querySelector('#boxes');
 
-// <div id="controls">
-//   <input type="number" min="1" max="100" step="1" />
-//   <button type="button" data-create>Create</button>
-//   <button type="button" data-destroy>Destroy</button>
-// </div>
+const inputEl = document.querySelector('input');
 
-// <div id="boxes"></div>
+const createBtn = document.querySelector('button[data-create]');
 
-// Создай функцию createBoxes(amount), которая принимает один параметр - число. Функция создает столько <div>, сколько указано в amount и добавляет их в div#boxes.
+const destroyBtn = document.querySelector('button[data-destroy]');
 
-// Размеры самого первого <div> - 30px на 30px.
-// Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
-// Все элементы должены иметь случайный цвет фона в формате HEX. Используй готовую функцию getRandomHexColor для получения цвета.
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
+let boxSize = 20;
 
-// Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы.
+function createBoxes(amount) {
+  divEl.innerHTML = '';
+
+  amount = Number(inputEl.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    const box = document.createElement('div');
+    box.style.backgroundColor = getRandomHexColor();
+    boxSize += 10;
+    box.style.width = box.style.height = boxSize + 'px';
+    divEl.append(box);
+  }
+
+  boxSize = 20;
+}
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+  divEl.innerHTML = '';
+}
