a451af365d68ac6579dbf28a14741cf6b6a5983a
igr-0k <igor.01101110@gmail.com>
2022-09-22 16:27:15 +0300

    Alt. Task#5

--- js/task-05.js
+++ js/task-05.js
@@ -2,8 +2,10 @@ const input = document.querySelector('#name-input');
 
 const output = document.querySelector('#name-output');
 
+// input.addEventListener('input', event => {
+//   output.textContent = event.currentTarget.value ? event.currentTarget.value.trim() : 'Anonymous';
+// });
+
 input.addEventListener('input', event => {
-  event.currentTarget.value === ''
-    ? (output.textContent = 'Anonymous')
-    : (output.textContent = event.currentTarget.value.trim());
+  output.textContent = event.currentTarget.value.trim() || 'Anonymous';
 });
